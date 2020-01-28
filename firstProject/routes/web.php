286d0d6755b2c4440d5c8cfc038ba6ebafeb8ff3
Anfaza <a.frayhi@gmail.com>
2020-01-28 13:18:58 +0100

    experiment

--- firstProject/routes/web.php
+++ firstProject/routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('test',function(){
+    $name = request('name');
+
+return view('test',[
+    'name'=> request ('name')
+    ]);
 });
